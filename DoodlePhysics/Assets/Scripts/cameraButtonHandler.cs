4f2a5e204c95d056644d6a66d2c8e580df35abe0
Brett Hoelscher <bretthoelscher@Bretts-MacBook-Pro.local>
2019-10-29 21:12:29 -0500

    import image capture scene and scripts

---
+++ DoodlePhysics/Assets/Scripts/cameraButtonHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cameraButtonHandler : MonoBehaviour
+{
+    public void captureDoodle(RawImage background)
+    {
+        #region image capture
+        WebCamTexture capture = (UnityEngine.WebCamTexture)background.texture;
+        Color[] Pixels = capture.GetPixels();
+        bool[,] binArray = new bool[capture.width, capture.height];
+
+        Debug.Log(capture.width);
+        Debug.Log(capture.height);
+
+        for (int i = 0; i < Pixels.Length; i++)
+        {
+            float avgColor = (Pixels[i][0] + Pixels[i][1] + Pixels[i][2]) / 3;
+            if (avgColor >= 0.3)
+            {
+                binArray[(capture.width - 1) - (i % capture.width), i / capture.width] = false;
+            }
+            else
+            {
+                binArray[(capture.width - 1) - (i % capture.width), i / capture.width] = true;
+            }
+        }
+        #endregion
+
+        #region array output
+        string row;
+        for (int i = 0; i < capture.width; i++)
+        {
+            row = "";
+            for (int j = 0; j < capture.height; j++)
+            {
+                if (binArray[i, j])
+                {
+                    row += "1";
+                }
+                else
+                {
+                    row += "0";
+                }
+            }
+            Debug.Log(row);
+        }
+        #endregion
+    }
+}
