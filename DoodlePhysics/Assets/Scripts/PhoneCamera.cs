f1bf3e2cd79ed60016763c0a59eabafd27671092
Ross <VicisVitium@gmail.com>
2019-11-01 15:51:00 -0700

    Refactor code to show camera using Unity Remote 5
    PhoneCamera.cs code to find phone camera was in the Start() function, but the phone camera isn't immediately available.
    
    Refactored code so that code looks for and sets back camera on every Update() cycle.

--- DoodlePhysics/Assets/Scripts/PhoneCamera.cs
+++ DoodlePhysics/Assets/Scripts/PhoneCamera.cs
@@ -15,36 +15,7 @@ public class PhoneCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        defaultBackground = background.texture;
-        WebCamDevice[] devices = WebCamTexture.devices;
 
-        if (devices.Length == 0)
-        {
-            Debug.Log("No camera detected");
-            camAvailable = false;
-            return;
-        }
-
-        for (int i = 0; i < devices.Length; i++)
-        {
-            Debug.Log(devices[i].name);
-            if (devices[i].name == "Back Camera")
-            {
-                Debug.Log(devices[i].name);
-                backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
-            }
-        }
-
-        if (backCam == null)
-        {
-            Debug.Log("Unable to find back camera");
-            return;
-        }
-
-        backCam.Play();
-        background.texture = backCam;
-
-        camAvailable = true;
     }
 
     // Update is called once per frame
@@ -52,18 +23,49 @@ public class PhoneCamera : MonoBehaviour
     {
         if (!camAvailable)
         {
-            return;
+            defaultBackground = background.texture;
+            WebCamDevice[] devices = WebCamTexture.devices;
+
+            if (devices.Length == 0)
+            {
+                Debug.Log("No camera detected");
+                camAvailable = false;
+                return;
+            }
+
+            for (int i = 0; i < devices.Length; i++)
+            {
+                Debug.Log(devices[i].name);
+                if (devices[i].name == "Back Camera" || devices[i].name == "Remote Back Camera")
+                {
+                    Debug.Log(devices[i].name);
+                    backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+                }
+            }
+
+            if (backCam == null)
+            {
+                Debug.Log("Unable to find back camera");
+                return;
+            }
+
+            backCam.Play();
+            background.texture = backCam;
+
+            camAvailable = true;
         }
 
-        float ratio = (float)backCam.width / (float)backCam.height;
-        fit.aspectRatio = ratio;
+        if (camAvailable == true)
+        {
+            float ratio = (float)backCam.width / (float)backCam.height;
+            fit.aspectRatio = ratio;
 
-        float scaleY = backCam.videoVerticallyMirrored ? -1f : 1f;
-        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+            float scaleY = backCam.videoVerticallyMirrored ? -1f : 1f;
+            background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
 
-        int orient = -backCam.videoRotationAngle;
-        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
-
+            int orient = -backCam.videoRotationAngle;
+            background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+        }
 
 
         //UnityEngine.Texture2D newTexture = (UnityEngine.Texture2D)defaultBackground;
